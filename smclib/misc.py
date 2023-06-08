82c1e96fa0bf38091d6dc790cf3760ee0ac50cdc
Josh Hug <hug@cs.berkeley.edu>
2023-06-07 22:29:31 -0700

    Add answers to module two task one

---
+++ smclib/misc.py
@@ -0,0 +1,12 @@
+def reveal_answer_to_module_two_task_one():
+	print("""The third possibility is rotating your head, i.e. rolling it from lying 
+on your left shoulder to your right shoulder. 
+
+There are others answers that might have been tempting, e.g. moving your nose forward or backward,
+moving your head left to right (also known in dance as a "head slide"), or even standing up or sitting down,
+which technically moves your head up and down. These three degrees of freedom are not orientation, but rather
+position. That is, they say where you head is, and not how it is oriented.
+
+Thus there are a total of six degrees of freedom for your head, three related to
+orientation, and three related to position.
+""")
